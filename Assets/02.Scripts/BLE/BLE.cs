52d745a8d8c5e5d6310652a6b0b7de620a666659
boracles <100pour100@naver.com>
2022-07-06 22:16:22 +0900

    BLE script

--- Assets/02.Scripts/BLE/BLE.cs
+++ Assets/02.Scripts/BLE/BLE.cs
@@ -3,46 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 using ArduinoBluetoothAPI;
 using UnityEngine.Android;
+using TMPro;
 
 public class BLE : MonoBehaviour
 {
     private BluetoothHelper helper;
-    private static string serviceUUID = "FFA0";
-    private static string characteristicUUID = "FFA1";
+    private static string serviceUUID = "00001800-0000-1000-8000-00805f9b34fb";
+    private static string characteristicUUID = "00002a00-0000-1000-8000-00805f9b34fb";
     private BluetoothHelperCharacteristic bluetoothHelperCharacteristic;
-    void Start()
+    
+    // 싱글턴 인스턴스 선언
+    public static BLE instance = null;
+    
+    // 블루투스로 들어오는 값을 연결할 변수
+    public TMP_Text bleMsg;
+    public string message;
+    private string tmp;
+    
+    // 스크립트가 실행되면 가장 먼저 호출되는 유니티 이벤트 함수
+    private void Awake()
+    {
+        // instance가 할당되지 않았을 경우
+        if (instance == null)
+        {
+            instance = this;
+        }
+        // instance에 할당된 클래스의 인스턴스가 다를 경우
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        // 다른씬으로 넘어가더라도 삭제하지 않고 유지
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
     {
         BluetoothHelper.BLE = true;
-        helper = BluetoothHelper.GetInstance("raspberrypi");
+        helper = BluetoothHelper.GetInstance("BORATOOTH");
         helper.OnScanEnded += OnScanEnded;
         helper.OnConnected += OnConnected;
+        helper.OnDataReceived += OnMessageReceived;
         helper.OnConnectionFailed += OnConnectionFailed;
         helper.OnCharacteristicChanged += OnCharacteristicChanged;
         helper.OnCharacteristicNotFound += OnCharacteristicNotFound;
         helper.OnServiceNotFound += OnServiceNotFound;
+        
         helper.ScanNearbyDevices();
+        
+        helper.setTerminatorBasedStream("\n");
         
         Permission.RequestUserPermission(Permission.CoarseLocation);
         bluetoothHelperCharacteristic = new BluetoothHelperCharacteristic(characteristicUUID, serviceUUID);
     }
 
-    void OnScanEnded(BluetoothHelper helper, LinkedList<BluetoothDevice> devices)
+    private void OnScanEnded(BluetoothHelper helper, LinkedList<BluetoothDevice> devices)
     {
-        if(helper.isDevicePaired())
+        if (helper.isDevicePaired())
+        {
             helper.Connect();
+        }
         else
+        {
             helper.ScanNearbyDevices();
+        }
     }
 
+    private void WriteMsg(string msg)
+    {
+        tmp += $"> {msg} \n";
+        bleMsg.text = tmp;
+    }
+
+    private void OnMessageReceived(BluetoothHelper helper)
+    {
+        message = helper.Read();
+        WriteMsg($"들어온 값 : {message}");
+    }
+    
     void OnConnected(BluetoothHelper helper)
     {
         List<BluetoothHelperService> services = helper.getGattServices();
         foreach (BluetoothHelperService s in services)
         {
-            Debug.Log($"Service : [{s.getName()}]");
+            WriteMsg($"Service : [{s.getName()}]");
             foreach (BluetoothHelperCharacteristic c in s.getCharacteristics())
             {
-                Debug.Log($"Characteristic : [{c.getName()}]");
+                WriteMsg($"Characteristic : [{c.getName()}]");
             }
         }
         helper.Subscribe(bluetoothHelperCharacteristic);
@@ -50,7 +97,7 @@ public class BLE : MonoBehaviour
 
     void OnConnectionFailed(BluetoothHelper helper)
     {
-        Debug.Log("Connection failed");
+        WriteMsg("블루투스 연결 실패");
         helper.ScanNearbyDevices();
     }
 
@@ -75,7 +122,12 @@ public class BLE : MonoBehaviour
         helper.WriteCharacteristic(bluetoothHelperCharacteristic, data);
     }
 
-    void OnDestroy()
+    public void SendData(int i)
+    {
+        helper.SendData(i.ToString());
+    }
+
+    private void OnDestroy()
     {
         helper.OnScanEnded -= OnScanEnded;
         helper.OnConnected -= OnConnected;
@@ -83,6 +135,7 @@ public class BLE : MonoBehaviour
         helper.OnCharacteristicChanged -= OnCharacteristicChanged;
         helper.OnCharacteristicNotFound -= OnCharacteristicNotFound;
         helper.OnServiceNotFound -= OnServiceNotFound;
+        helper.OnDataReceived -= OnMessageReceived;
         helper.Disconnect();
     }
 }
