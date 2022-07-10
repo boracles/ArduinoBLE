a9a85ce585e26d95bf5992901229919285a9d77b
boracles <atelier@boracles.art>
2022-07-11 03:50:49 +0900

    BPM Ctrl 스크립트 작성중

---
+++ Assets/02.Scripts/BPMCtrl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class BPMCtrl : MonoBehaviour
+{
+    private BLE bleMgr;
+    public Material wallMat;
+    public GameObject bulb;
+}
