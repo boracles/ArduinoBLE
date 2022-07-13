21fb89bb3ffa0620ce4adb8fe03a75bab1507e64
Bora Youn <100pour100@naver.com>
2022-07-13 21:46:34 +0900

    at command script 수정

--- Assets/02.Scripts/ATCommand/ATCommand.ino
+++ Assets/02.Scripts/ATCommand/ATCommand.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>     // 소프트웨어 시리얼 통신 라이브러리 호출
 
-SoftwareSerial BTSerial(2, 3);  // 소프트웨어 시리얼포트 (TX, RX)
+SoftwareSerial BTSerial(7, 8);  // 소프트웨어 시리얼포트 (TX, RX)
 
 void setup()
 {
@@ -8,7 +8,7 @@ void setup()
   Serial.println("아두이노가 준비되었습니다.");
   Serial.println("시리얼 모니터에서 Both NL & CR 을 선택하세요.");
   
-  BTSerial.begin(38400); // 블루투스 모듈과의 시리얼 통신 초기화
+  BTSerial.begin(9600); // 블루투스 모듈과의 시리얼 통신 초기화
 }
 
 void loop() 
